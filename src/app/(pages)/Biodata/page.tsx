abab0da30dcd59bb2572c7d9f6ee4b444aabb2f1
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-04-10 10:47:17 +0700

    feat:installasi page biodata

---
+++ src/app/(pages)/Biodata/page.tsx
@@ -0,0 +1,14 @@
+const Biodata = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="grid grid-cols-1 grid-rows-2 gap-4" title="background">
+        <div className="bg-white" title="white"></div>
+        <div
+          className="bg-sky-500 w-[100vw] h-[50vh] rounded-t-[6rem]"
+          title="blue"
+        ></div>
+      </div>
+    </div>
+  );
+};
+export default Biodata;
