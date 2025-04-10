09344e400b5c48869e5d307e2225b899dbb115cd
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-04-10 10:56:19 +0700

    feat:fixed

--- src/app/(pages)/Biodata/page.tsx
+++ src/app/(pages)/Biodata/page.tsx
@@ -1,14 +1,20 @@
 const Biodata = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="grid grid-cols-1 grid-rows-2 gap-4" title="background">
-        <div className="bg-white" title="white"></div>
-        <div
-          className="bg-sky-500 w-[100vw] h-[50vh] rounded-t-[6rem]"
-          title="blue"
-        ></div>
+    <>
+      <div className="w-screen h-screen relative">
+        <div className="grid grid-cols-1 grid-rows-2 gap-4" title="background">
+          <div className="bg-white" title="white"></div>
+          <div
+            className="bg-sky-500 w-[100vw] h-[50vh] rounded-t-[6rem]"
+            title="blue"
+          ></div>
+        </div>
+        <div className="absolute inset-0 flex justify-center items-center flex-col">
+          <div className=""></div>
+          <div className=""></div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Biodata;
