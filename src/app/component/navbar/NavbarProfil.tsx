80ce24eb7597acb3faf3c767323ffa44178f4f0c
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-04-12 03:33:42 +0700

    feat:installation navbar Profile

---
+++ src/app/component/navbar/NavbarProfil.tsx
@@ -0,0 +1,40 @@
+import iconHItam from "../../../../public/asset/IconHitam.png";
+import Image from "next/image";
+import profil from "../../../../public/asset/porfil.png";
+import Link from "next/link";
+import { useHook } from "@/app/layout/Provider";
+
+const NavbarProfil: React.FC = () => {
+  const { currentUser, isLoading } = useHook();
+
+  return (
+    <div className="flex justify-around pt-[1rem] pb-[1rem] border-b-1">
+      <Link href="/Home">
+        <div className="flex">
+          <Image
+            src={iconHItam}
+            alt="iconHitam"
+            className="w-[3vw] h-[5vh]"
+          ></Image>
+          <h1 className="font-bold text-black text-[2rem]">Kosthub</h1>
+        </div>
+      </Link>
+      <Link href="/Home">
+        <div className="flex gap-2 items-center">
+          <div className="border-2 rounded-full p-1 hover:bg-sky-500 duration-[0.5s] hover:text-white">
+            <h1 className="font-bold">Beranda</h1>
+          </div>
+          <Image
+            src={profil}
+            alt="profil"
+            className="w-[2vw] h-[3.8vh]"
+          ></Image>
+          <h1 className="font-bold">
+            {isLoading ? "Loading..." : currentUser?.username ?? "Guest"}
+          </h1>
+        </div>
+      </Link>
+    </div>
+  );
+};
+export default NavbarProfil;
