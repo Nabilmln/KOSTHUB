e4fdd71360b30eaf2ce30d5a26d1b777f24e9ce2
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-05-08 04:59:33 +0700

    feat: Rename Base url to localHost

--- src/app/util/API.ts
+++ src/app/util/API.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
 
 const API = axios.create({
-  baseURL: "https://kosthub.vercel.app",
+  baseURL: "http://localhost:5000/",
 });
 
 API.interceptors.request.use(
